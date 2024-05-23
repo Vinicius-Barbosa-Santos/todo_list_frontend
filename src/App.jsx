b461f1180711eea1da570eebc80b9fe96561f304
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-05-23 15:31:24 -0300

    feat: fetch tasks from back-end, clean useless files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { api } from "./services/axios";
+import TaskItem from "./components/TaskItem";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchGetTasks = async () => {
+    try {
+      const response = await api.get("/tasks");
+      console.log(response.data)
+      setTasks(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchGetTasks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {tasks.map((task) => (
+        <TaskItem key={task._id} task={task} />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
